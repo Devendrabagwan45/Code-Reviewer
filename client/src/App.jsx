17d5af03b95781b9dbc16c38ad3ab09e0dad7f3c
Devendra Bagwan <devendrabagwan78@gmail.com>
2025-07-22 13:27:00 +0530

    so

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,9 +17,12 @@ const App = () => {
   }, []);
 
   async function reviewCode() {
-    const response = await axios.post("/api/ai/get-review", {
-      code,
-    });
+    const response = await axios.post(
+      "https://code-reviewer-dw9b.vercel.app/api/ai/get-review",
+      {
+        code,
+      }
+    );
     setReview(response.data);
   }
   return (
